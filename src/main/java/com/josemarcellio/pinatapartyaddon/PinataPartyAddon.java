96a258efff457ffe8dd7f952540ef410ef74ef7a
josemarcellio <josemarcellio01@gmail.com>
2024-10-04 22:07:11 +0800

    Add bStats

--- src/main/java/com/josemarcellio/pinatapartyaddon/PinataPartyAddon.java
+++ src/main/java/com/josemarcellio/pinatapartyaddon/PinataPartyAddon.java
@@ -3,6 +3,7 @@ package com.josemarcellio.pinatapartyaddon;
 import com.josemarcellio.pinatapartyaddon.command.PinataPartyAddonReloadCommand;
 import com.josemarcellio.pinatapartyaddon.handler.DisguiseHandler;
 import com.josemarcellio.pinatapartyaddon.listener.PinataPartyListener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PinataPartyAddon extends JavaPlugin {
@@ -10,6 +11,11 @@ public class PinataPartyAddon extends JavaPlugin {
     @SuppressWarnings("ConstantConditions")
     @Override
     public void onEnable() {
+        getLogger().info("PinataPartyAddon by JoseMarcellio enabled!");
+
+        int id = 23535;
+        new Metrics(this, id);
+
         saveDefaultConfig();
         DisguiseHandler disguiseHandler = new DisguiseHandler(this);
         getServer().getPluginManager().registerEvents(new PinataPartyListener(disguiseHandler), this);
@@ -19,6 +25,6 @@ public class PinataPartyAddon extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        super.onDisable();
+        getLogger().info("PinataPartyAddon by JoseMarcellio disabled!");
     }
 }
